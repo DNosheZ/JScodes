ee8bdf6aed98673b0d2d68ad7ebb1e7a0d20da5a
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-07-04 12:46:50 -0500

    listBetween2Numbers.js

--- listBetween2Numbers.js
+++ listBetween2Numbers.js
@@ -1,7 +1,6 @@
 'use strict'
 var num1=parseInt(prompt('Ingrese el menor de los numeros',''));//inputing the start number
 var num2=parseInt(prompt('Ingrese el mayor de los numeros',''));//inputing the finish number
-var cont=0
 document.write('<h1>De '+num1+' a '+num2+' hay '+(num2-num1)+' numeros y son:</h1>');//
 for(var i=num1;i<num2;i++){
     document.write(i+'<br/>');//showing in vertical list the numbers between the inputed, counting the first & the last
