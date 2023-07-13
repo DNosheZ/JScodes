0038d16881d94b387396cb72b5c93ce4ddd9f4b7
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-07-13 14:17:41 -0500

    Delete 14-dates.js

--- 14-dates.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var fecha = new Date();//obtenemos la fecha actual
-var year=fecha.getFullYear();
-var month=parseInt(fecha.getMonth())+1;/*dado que JS empieza a contar los meses desde 0, 
-tenemos que sumar 1*/
-var day=fecha.getDate();
-console.log(fecha);
-console.log(year,month,day);
