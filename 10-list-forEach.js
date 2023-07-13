703f830a4866042491f3f9f7d78dbcd4b57ea105
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2023-07-13 14:19:01 -0500

    Delete 10-list-forEach.js

--- 10-list-forEach.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-var novias=['Maria Jose','Brisa','Lorena','Maye','Michel','Valentina','Daniela','Madison','Andrea'];
-document.write('<h1>Top de chicas con las que minimamente me he besado</h1>');//actualizare esto de vez en cuando jsjsjs
-document.write('<ul>');
-novias.forEach((novia,puesto)=>{
-    document.write('<li>'+(puesto+1)+' '+novia+'</li>');
-});
-document.write('</ul>');
